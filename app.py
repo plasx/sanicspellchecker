0036dacbba1fe262064cc71bc53755399ec1a0ca
plasx <plasx@live.com>
2019-04-01 17:58:43 -0400

    Got rid of sanic redis (aioredis wrapper) due to it not functioning well, i'll need to dive into it later to see how I can make it work, added logic  and 404 for finding mixcasing

--- app.py
+++ app.py
@@ -1,58 +1,50 @@
 from sanic import Sanic
 from sanic.response import json
-from sanic_redis import SanicRedis
+from sanic.exceptions import abort
+import redis
 
 app = Sanic()
-app.config.update(
-    {
-        'REDIS': {
-            'address': ('redis', 6379),
-            'db': 0
-        }
-    }
-)
-
-redis = SanicRedis(app)
-
 
 def conforms(word):
     if word == word.upper() or word == word.lower():
-        return True
+        return "compliant"
     else:
-        for i in range(1, len(word)):
-            if word[i].isupper():
-                return False
-    return True
+        for i in word[1:-1]:
+            if i.isupper():
+                return "noncompliant"
+    if word[-1:].isupper():
+        return "suggest"
+    return "compliant"
 
 
 def suggestions(word):
-    with await redis.conn as r:
-        result = await r.sismember(word[0], word)
-        if result == 0:
-            recommendedwords = [user for user in r.sscan_iter(word[0], match=f'{word}*')]
-        if not recommendedwords and len(word) > 3:
-            await suggestions(word[:-1])
-        else:
-            return recommendedwords
+    r = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
+    recommendedwords = [user for user in r.sscan_iter(word[0], match=f'{word}*')]
+    if not recommendedwords and len(word) > 2:
+        suggestions(word[:-1])
+    else:
+        return recommendedwords or []
 
-@app.route('/')
-async def test(request):
-    return json({'hello': 'world'})
 
 @app.route('/spellcheck/<word>')
 async def spellchecker(request, word):
-    if conforms(word):
-        with await redis.conn as r:
-            result = await r.sismember(word[0], word)
-            redis_check = "true" if result == 1 else "false"
-            if redis_check == "false":
-                import ipdb; ipdb.sset_trace()
-                recommendedwords = [user for user in r.sscan_iter(word[0], match=f'{word}*')]
-                print(recommendedwords)
-                return json({"suggestions": [], "correct": redis_check})
-        return json({"suggestions": [], "correct": redis_check})
+    # if word == 'car':
+    #     import ipdb; ipdb.sset_trace()
+    r = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
+    if conforms(word) == "compliant":
+        word = word.lower()
+        result = r.sismember(word[0], word)
+        redis_check = "true" if result is True else "false"
+        if redis_check == "false":
+            recommendedwords = [user for user in r.sscan_iter(word[0], match=f'{word}*')]
+            return json({"suggestions": recommendedwords or [], "correct": redis_check})
+        else:
+            return json({"suggestions": [], "correct": "true"})
+    elif conforms(word) == "suggest":
+        word = word.lower()
+        return json({"suggestions": suggestions(word) or [], "correct": "false"})
     else:
-        return json({"suggestions": [], "correct": redis_check})
+        abort(404)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
